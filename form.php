4ddef9e4e179a69eb245675dd0ccdfa1b25b5598
youwanpeter <youwanpeter226@gmail.com>
2023-11-10 21:16:07 +0530

    second commit

--- form.php
+++ form.php
@@ -7,7 +7,7 @@
 	$message = isset( $_POST['feedbackMessage'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['feedbackMessage'] ) : "";
 
 	//Headers
-	$to = "youremail@domain.com";
+	$to = "youwanpeter226@gmail.com";
     $subject = 'Contact Us';
 	$headers = "MIME-Version: 1.0\r\n";
 	$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
